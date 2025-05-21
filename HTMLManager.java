47f569a65e7faf4e9948aa67ea75f7705c4cd4f4
y53y7 <maxwell.sutton@go.shoreline.edu>
2025-05-21 11:14:35 -0700

    Created HTMLManager method

--- HTMLManager.java
+++ HTMLManager.java
@@ -4,19 +4,25 @@ public class HTMLManager {
   private Queue<HTMLTag> tags;
   
   public static void HTMLManager(Queue<HTMLTag> html) {
-     
+     Queue<HTMLTag> newQueue = new LinkedList<>();
+     if(html.size() == 0) {
+        throw new IllegalArgumentException("The queue is empty");
+     } else {
+        while(!html.isEmpty()) {
+           newQueue.add(html.remove());
+        }
+     }
   }
   
-  public static Queue<HTMLTag> getTags {
+  public static Queue<HTMLTag> getTags() {
      return null;
   }
   
-  public static String toString() {
+  public String toString() {
      return "";
   }
   
   public static void fixHTML() {
-  
   }
 }
 
