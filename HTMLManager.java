b1213c75c6620fa2366e30d08ea6ce8d2ef7c8b6
y53y7 <maxwell.sutton@go.shoreline.edu>
2025-05-21 11:00:20 -0700

    Initial Files

---
+++ HTMLManager.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class HTMLManager {
+  private Queue<HTMLTag> tags;
+  
+  public static void HTMLManager(Queue<HTMLTag> html) {
+     
+  }
+  
+  public static Queue<HTMLTag> getTags {
+     return null;
+  }
+  
+  public static String toString() {
+     return "";
+  }
+  
+  public static void fixHTML() {
+  
+  }
+}
+
